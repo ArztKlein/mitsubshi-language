d9649b9a610f41f7e85123d44fdb37ae7c1134f1
ArztKlein <52363453+ArztKlein@users.noreply.github.com>
2022-06-08 12:33:03 +1200

    basic math

--- mitsubishi_language/__main__.py
+++ mitsubishi_language/__main__.py
@@ -7,17 +7,30 @@ class MitsubishiLanguage:
     SERVICE_REGEX = '(' + '|'.join([service.name() for service in SERVICES]) + ')'
 
 class CalcLexer(Lexer):
-    tokens = {STRING, SERVICE, INSERT}
+
+    def __init__(self):
+         self.nesting_level = 0
+
+    tokens = {STRING, SERVICE, INSERT, NUMBER, PLUS, MINUS, TIMES, DIVIDE, POWER, ASSIGN, LPAREN, RPAREN, IDENTIFIER}
     ignore = ' \t'
 
+    literals = { '(', ')', '{', '}', '.', '!' }
+
     # Tokens
-    STRING = r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')'''
+    ASSIGN  = r'='
+    DIVIDE  = r'/'
+    STRING  = r'''("[^"\\]*(\\.[^"\\]*)*"|'[^'\\]*(\\.[^'\\]*)*')'''
+    INSERT  = r'->'
+    MINUS   = r'-'
+    NUMBER  = r'\d+'
+    PLUS    = r'\+'
+    TIMES   = r'\*'
+    POWER   = r'\^'
+    LPAREN  = r'\('
+    RPAREN  = r'\)'
 
     # Generate the regex for a service
     SERVICE = MitsubishiLanguage.SERVICE_REGEX
-    
-    # Special symbols
-    INSERT = r'->'
 
     # Ignored pattern
     ignore_newline = r'\n+'
@@ -25,11 +38,25 @@ class CalcLexer(Lexer):
     # Extra action for newlines
     def ignore_newline(self, t):
         self.lineno += t.value.count('\n')
+    
+    @_(r'\{')
+    def lbrace(self, t):
+        t.type = '{'      # Set token type to the expected literal
+        self.nesting_level += 1
+        return t
+
+    @_(r'\}')
+    def rbrace(self, t):
+        t.type = '}'      # Set token type to the expected literal
+        self.nesting_level -=1
+        return t
 
     @_("string")
     def STRING(self, t):
         t.value = self.remove_quotes(t.value)
         return t
+
+    IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
     def remove_quotes(self, text: str):
         if text.startswith('\"') or text.startswith('\''):
@@ -43,6 +70,18 @@ class CalcLexer(Lexer):
 class CalcParser(Parser):
     tokens = CalcLexer.tokens
 
+    precedence = (
+        ('left', PLUS, MINUS),
+        ('left', TIMES, DIVIDE),
+        ('right', POWER)
+    )
+
+    def __init__(self):
+        self.services = {}
+
+        for service in MitsubishiLanguage.SERVICES:
+            self.services[service.name()] = service
+
     @_("value INSERT SERVICE")
     def statement(self, p):
         return self.get_service(p.SERVICE).insert(p.value)
@@ -51,15 +90,25 @@ class CalcParser(Parser):
     def statement(self, p):
         return self.get_service(p.SERVICE).insert(p.statement)
 
+    @_("value POWER value")
+    def value(self, p):
+        return p.value0 ** p.value1
+
+    @_("value TIMES value")
+    def value(self, p):
+        return p.value0 * p.value1
+    
+    @_("value PLUS value")
+    def value(self, p):
+        return p.value0 + p.value1
+
+    @_("NUMBER")
+    def value(self, p):
+        return int(p.NUMBER)
+
     @_("STRING")
     def value(self, p):
         return p.STRING
-
-    def __init__(self):
-        self.services = {}
-
-        for service in MitsubishiLanguage.SERVICES:
-            self.services[service.name()] = service
 
     def get_service(self, name):
         return self.services[name]
